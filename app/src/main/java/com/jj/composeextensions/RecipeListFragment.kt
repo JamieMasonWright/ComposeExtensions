109d377079ea0e3bd3f20b22079c8109aa38487c
jamie <bluegigsolutions@gmail.com>
2022-08-03 09:33:40 +0100

    Initial commit

---
+++ app/src/main/java/com/jj/composeextensions/RecipeListFragment.kt
@@ -0,0 +1,21 @@
+package com.jj.composeextensions
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class RecipeListFragment :Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_recipe_list, container, false)
+
+    }
+    }
